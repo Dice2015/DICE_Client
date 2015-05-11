21437911bf7371289f2778d264fd4272a9834b4b
Tobias <uk014965@student.uni-kassel.de>
2015-05-11 15:31:59 +0200

    added sample preferences screen

---
+++ diceplugin/src/de/uks/se/scoreproject/dice/preferences/PreferenceConstants.java
@@ -0,0 +1,20 @@
+package de.uks.se.scoreproject.dice.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String P_PATH = "pathPreference";
+
+	public static final String P_BOOLEAN = "booleanPreference";
+
+	public static final String P_CHOICE = "choicePreference";
+
+	public static final String P_STRING_username = "usernamestringPreference";
+	
+	public static final String P_STRING_Address = "addrstringPreference";
+	
+	public static final String P_STRING_pw = "pwstringPreference";
+	
+}
---
+++ diceplugin/src/de/uks/se/scoreproject/dice/preferences/PreferenceInitializer.java
@@ -0,0 +1,30 @@
+package de.uks.se.scoreproject.dice.preferences;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import dice.Activator;
+
+/**
+ * Class used to initialize default preference values.
+ */
+public class PreferenceInitializer extends AbstractPreferenceInitializer {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences() {
+		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+		store.setDefault(PreferenceConstants.P_BOOLEAN, true);
+		store.setDefault(PreferenceConstants.P_CHOICE, "choice2");
+		store.setDefault(PreferenceConstants.P_STRING_Address,
+				"localhost:8089");
+		store.setDefault(PreferenceConstants.P_STRING_username,
+				"benutzername");
+		store.setDefault(PreferenceConstants.P_STRING_pw,
+				"123456");
+	}
+
+}
